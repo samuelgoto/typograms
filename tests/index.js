b178987f6c187fb26933a3e2cf754687d129e5a2
Sam Goto <samuelgoto@gmail.com>
2022-08-23 17:17:09 -0700

    Start adding the infrastructure for testing and command line execution

---
+++ tests/index.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+describe("Typograms", function() {
+
+  it("", () => {
+  });
+  
+  function assertThat(x) {
+    return {
+      equalsTo(y) {
+        assert.deepEqual(x, y);
+      }
+    }
+  }
+});
